7efe0fe0d4439fd7fffd4ae2a371997f78c3d95f
Valentijn Nieman <valentijnnieman@gmail.com>
2017-06-01 11:15:32 +0200

    changed to webpack dev server

--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const path = require('path')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].bundle.css",
+    disable: process.env.NODE_ENV === "development"
+});
 
 module.exports = {
   module: { 
-    loaders: [
+    rules: [ 
       {
         test: /\.jsx?$/,
         exclude: /(node_modules)/,
         loader: 'babel-loader',
         query: {
-          presets: ['es2015', 'react']
+          presets: ['es2015', 'react'],
+          plugins: ['transform-object-rest-spread']
         }
+      },
+      {
+        test: /\.scss$/,
+        use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
       }
     ]
   },
+  plugins: [
+    extractSass
+  ],
   entry: './src/main.js',
   output: {
-    filename: 'index.js',
-    path: path.resolve('./', 'dist')
-  },
-  plugins: [
-    new UglifyJSPlugin()
-  ]
+    path: path.resolve(__dirname, "dist"),
+    publicPath: '/dist/',
+    filename: "index.js"
+  }
 }
